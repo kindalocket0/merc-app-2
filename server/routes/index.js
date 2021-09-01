585a87fb3a3a0bdf22b99fc38ba4ec976f4994c7
kindalocket0 <59594958+kindalocket0@users.noreply.github.com>
2021-09-01 12:17:52 -0600

    serving dist index file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var router = express.Router()
+const path = require('path')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ a: 'test' })
+  console.log(__dirname)
+  res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'))
 })
 
 module.exports = router
